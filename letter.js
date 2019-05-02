20324de73975bc94c20d06a9091ec3e6d587fddb
unknown <evan2580@gmail.com>
2019-05-02 16:23:04 -0400

    first 2 files finished

--- letter.js
+++ letter.js
@@ -1,1 +1,19 @@
+function Letter (letter){
+  this.letter = letter
+  this.guessed = false; //boolean value
 
+  this.correctGuess = function(){
+    if (this.guessed) {
+      return this.letter;
+    } else {
+      return "_" ;
+    }
+  }
+
+  this.characterArgs = function(userGuess){
+    if (this.letter === userGuess){
+      this.guessed = true;
+  }
+}
+
+module.exports = Letter;
--- word.js
+++ word.js
@@ -1,1 +1,21 @@
+var Letter = require("letter");
 
+function Word(word){
+  this.letterArray = word.split("");
+
+  this.letterObjs = letterArray.map(function(ltr){
+    return new Letter (ltr);
+  })
+
+  this.answer = function() {
+
+   return this.letterObjs.map(function(ltrObj){
+     return ltrObj.correctGuess();
+    }).join("")
+  }
+  this.callGuess = function(userGuess){
+    this.letterObjs.forEach(ltrObj => {
+      ltrObj.characterArgs(userGuess);
+    });
+  }
+}
