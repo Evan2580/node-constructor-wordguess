9f3f94275622bc61a6b5d30a9c5a59ced07800e9
unknown <evan2580@gmail.com>
2019-05-04 19:27:32 -0400

    last push not completed

--- word.js
+++ word.js
@@ -1,21 +1,23 @@
-var Letter = require("letter");
+var Letter = require("letter");  //imports the other file letter.js
 
 function Word(word){
-  this.letterArray = word.split("");
+  this.letterArray = word.split(""); //splits array into string
 
-  this.letterObjs = letterArray.map(function(ltr){
+  this.letterObjs = letterArray.map(function(ltr){ //loops through the array using the map function! creates the objects for the word with NEW Letter syntax for each.
     return new Letter (ltr);
-  })
+  });
 
-  this.answer = function() {
+  this.answer = function() { //stores return in this.answer joins the letter objects "c""a""t" for example and checks to see if it is the correct guess
 
    return this.letterObjs.map(function(ltrObj){
      return ltrObj.correctGuess();
-    }).join("")
+    }).join("");
   }
-  this.callGuess = function(userGuess){
+  this.callGuess = function(userGuess){  //stores result in this.callGuess and converts from string to array
     this.letterObjs.forEach(ltrObj => {
       ltrObj.characterArgs(userGuess);
     });
   }
 }
+
+module.exports = Word; 
